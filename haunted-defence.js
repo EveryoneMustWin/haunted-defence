a4113c9f2740ea713d22d0f92be7a3468ddbd810
Oscar Gillespie <oscar.e.gillespie@gmail.com>
2018-10-27 16:17:38 +0100

    grid and timer in place

--- haunted-defence.js
+++ haunted-defence.js
@@ -1,5 +1,30 @@
 
 
 var hhtd = {
-    
+    width: 10,
+    height: 8
+}
+
+hhtd.init = function() {
+
+    var rowHtml;
+
+    for (var y = 0; y < hhtd.height; y++) {
+
+        rowHtml = "<div class='row'>";
+
+        for (var x = 0; x < hhtd.width; x++) {
+
+            console.log("another cell");
+            rowHtml += "<div class='cell'></div>";
+        }
+
+        rowHtml += "</div>";
+
+        $("#tracks").append(rowHtml);
+    }
+}
+
+hhtd.timer = function() {
+    console.log("timer");
 }
