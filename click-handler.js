62f8d98d03b9df37144648aa805e2e3ed76d220b
David Newell <david@danwell>
2018-10-27 20:19:55 +0100

    Implement shop and item selection

--- click-handler.js
+++ click-handler.js
@@ -2,5 +2,9 @@
 var clickHandler = {
     onClick: function (elem) {
         hhtd.decrementMoney(100);
+    },
+
+    shopItemClick: function (elem) {
+        hhtd.setActiveShopItem(this.getAttribute("shopItem"));
     }
 }
