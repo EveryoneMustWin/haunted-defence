0ea1fe0c6eb0a9f7b7dc6fa1f4771fd74e0e9758
David Newell <david@danwell>
2018-10-28 01:02:45 +0100

    Implement placement of scares

--- click-handler.js
+++ click-handler.js
@@ -1,7 +1,20 @@
 
 var clickHandler = {
     onClick: function (elem) {
-        hhtd.decrementMoney(100);
+        if(!hhtd.decrementMoney(100))
+            return;
+
+        if(hhtd.isCellOccupied(this))
+            return;
+
+        x = this.getAttribute("x");
+        y = this.getAttribute("y");
+
+        hhtd.layout[x,y] = hhtd.activeShopItem;
+        var newElem = document.createElement("div");
+        var itemClass = hhtd.activeShopItem.toLowerCase() + "-1";
+        newElem.className = itemClass;
+        this.appendChild(newElem);
     },
 
     shopItemClick: function (elem) {
